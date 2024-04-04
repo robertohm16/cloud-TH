203731680a5ceef83300d6ec7e965109c44e8975
robertohm16 <69583308+robertohm16@users.noreply.github.com>
2024-04-04 13:00:22 +0200

    Update server.py

--- server.py
+++ server.py
@@ -10,13 +10,10 @@ print("Message: '" + MESSAGE + "'")
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def root():
   print("Handling web request. Returning message.")
   result = MESSAGE.encode("utf-8")
   return result
-
-
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=PORT)
